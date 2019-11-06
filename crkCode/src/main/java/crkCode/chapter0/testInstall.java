3f16caf358840af9df7d9b450c28edf1a4b523f7
lampaban <francisco.lampaban@gmail.com>
2019-11-05 21:45:59 -0500

    seting up the dev environment

---
+++ crkCode/src/main/java/crkCode/chapter0/testInstall.java
@@ -0,0 +1,8 @@
+package crkCode.chapter0;
+
+public class testInstall {
+    public static void main(String args[])
+    {
+        System.out.println("Hello, World");
+    }
+}
---
+++ crkCode/src/main/java/crkCode/chapter1/test2.java
@@ -0,0 +1,8 @@
+package crkCode.chapter1;
+
+public class test2 {
+    public static void main(String args[])
+    {
+        System.out.println("Hello, World2");
+    }
+}
